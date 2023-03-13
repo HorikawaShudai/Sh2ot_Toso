58960eb815df7730e2b10318613d1e8349fabd90
yuki hayakawa <221017@st.yoshida-g.ac.jp>
2023-03-13 16:25:03 +0900

    ノイズの種類を大中小に

--- ソリューション/sound.cpp
+++ ソリューション/sound.cpp
@@ -45,6 +45,9 @@ SOUNDINFO g_aSoundInfo[SOUND_LABEL_MAX] =
 	{ "Data/SE/no_stamina.wav",0},				//息切れ音
 	{ "Data/SE/danger.wav",0},					//敵発券音
 	{ "Data/SE/death_1.wav",0},					//死亡音
+	{ "Data/SE/normal_noize.wav",0}.			//普通のノイズ
+	{ "Data/SE/small_noize.wav",0}.				//小さめのノイズ
+	{ "Data/SE/big_noize.wav",0}.				//大きいノイズ
 };
 
 //=============================================================================
